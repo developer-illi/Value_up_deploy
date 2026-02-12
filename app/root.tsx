f6068a567f08ce74964fc20dac289a1c71aff150
developer-illi <132191529+developer-illi@users.noreply.github.com>
2026-02-12 17:18:33 +0900

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
-        <script type='text/javascript' src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoAppKey}`} />
+        <script type='text/javascript' src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoAppKey}`} />
         <title>Value Up Partners</title>
       </head>
       <body>
